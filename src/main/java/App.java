4966cb27f7d7d74207364a1eb71e4eb0eb47753a
tots <rachealkari@gmail.com>
2022-07-21 01:18:13 +0300

    adding resources folders

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,8 @@
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
-
+        staticFileLocation("/public");
+        get("/hello", (request, response) -> "Hello Friend!");
     }
 }
