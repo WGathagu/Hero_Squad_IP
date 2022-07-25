a8eb9b47a3250ffbf72c10d8af13cb2a8b96ce4e
tots <rachealkari@gmail.com>
2022-07-25 13:26:43 +0300

    adding hero class

---
+++ src/main/java/Hero.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hero {
+        private String heroName;
+        private int heroage;
+        private String hero;
+        private String heroweakness;
+        private static List<Hero> heroEntry = new ArrayList<>();
+        private int heroID;
+        private String squadAlliance;
+}
