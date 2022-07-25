783a1683cfc53180b9ff7d32070f87b13032fa16
tots <rachealkari@gmail.com>
2022-07-25 13:44:27 +0300

    adding getter and setter methods

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -2,11 +2,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hero {
-        private String heroName;
-        private int heroage;
-        private String hero;
-        private String heroweakness;
-        private static List<Hero> heroEntry = new ArrayList<>();
-        private int heroID;
-        private String squadAlliance;
+    private String heroName;
+    private int heroAge;
+    private String heroSpecialPower;
+    private String heroWeakness;
+    private static List<Hero> heroEntry = new ArrayList<>();
+    private int heroID;
+    private String squadAlliance;
+
+    public Hero(String name, int age, String specialpower, String weakness) {
+        heroName = name;
+        heroAge = age;
+        heroSpecialPower = specialpower;
+        heroWeakness = weakness;
+    }
+        public void setPower(String power) {
+            heroSpecialPower = power;
+        }
+
+        public void setWeakness(String weakness) {
+            heroWeakness = weakness;
+        }
+
+        public String getName() {
+            return heroName;
+        }
+
+        public int getAge() {
+            return heroAge;
+        }
+
+        public String getPower() {
+            return heroSpecialPower;
+        }
+
+        public String getWeakness() {
+            return heroWeakness;
+        }
+
+        public int getHeroID() {
+            return heroID;
+        }
 }
