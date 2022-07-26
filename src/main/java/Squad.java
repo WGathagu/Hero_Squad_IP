ba301a87a5d525a667d5da6f97a5a28d18a1224f
tots <rachealkari@gmail.com>
2022-07-26 14:56:49 +0300

    adding squad class

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,4 +1,25 @@
 public class Squad {
     String squadName;
     String squadCause;
+    int squadID;
+
+    public Squad(String name, String cause, Hero hero) {
+        squadName = name;
+        squadCause = cause;
+
+    }
+
+    public String getName() {
+        return squadName;
+    }
+
+    public int getSquadId() {
+        return squadID;
+    }
+
+    public String getCause() {
+        return squadCause;
+    }
+
+
 }
