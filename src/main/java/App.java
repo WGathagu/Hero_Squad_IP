13408d68312551380b46dbd2497927f2fba93d1b
tots <rachealkari@gmail.com>
2022-07-26 14:58:42 +0300

    adding methods in squad class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,6 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-        get("/hello", (request, response) -> "Hello Friend!");
+
     }
 }
